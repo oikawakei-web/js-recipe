665dfead7dcfcb04408b82a00cf506dee07a8221
及川恵 <keitin10009@gmail.com>
2021-10-07 16:47:11 +0900

    [□と○]を学習しました。

--- 03_サンプル/01_□と○/main.js
+++ 03_サンプル/01_□と○/main.js
@@ -1,5 +1,29 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function() {
-  figure.classList.toggle("rounded")
+  if (figure.classList.contains("rounded")) {
+    figure.classList.remove("rounded")
+    figure.classList.add("triangle")
+  } else {
+    if (figure.classList.contains("triangle")) {
+      figure.classList.remove("triangle")
+      figure.classList.add("square")
+    } else {
+      figure.classList.add("rounded")
+    }
+  }
+}
+
+figure.onmouseover = function() {
+  if (figure.classList.contains("rounded")) {
+    figure.classList.remove("rounded")
+    figure.classList.add("triangle")
+  } else {
+    if (figure.classList.contains("triangle")) {
+      figure.classList.remove("triangle")
+      figure.classList.add("square")
+    } else {
+      figure.classList.add("rounded")
+    }
+  }
 }
